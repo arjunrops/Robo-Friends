33a1268a747e020d10a4783ced05ed890ea10104
arjunrops <55598748+arjunrops@users.noreply.github.com>
2020-08-07 13:57:24 +0530

    This project is made using React

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Goku',
+        username: 'thebest',
+        email: 'gogoku@june.com'
+    },
+    {
+        id: 2,
+        name: 'Vegita',
+        username: 'the2best',
+        email: 'vegs@may.com'
+    },
+    {
+        id: 3,
+        name: 'Gohan',
+        username: 'the3best',
+        email: 'gohan@january.com'
+    },
+    {
+        id: 4,
+        name: 'John Wick',
+        username: 'whereismydog',
+        email: 'johnwk@july.com'
+    },
+    {
+        id: 5,
+        name: 'Piccolo',
+        username: 'picklo',
+        email: 'picolo@november.com'
+    },
+    {
+        id: 6,
+        name: 'Quagmire',
+        username: 'alrigghttt',
+        email: 'giggidy@october.com'
+    },
+    {
+        id: 7,
+        name: 'Rick Sanchez',
+        username: 'picklerick',
+        email: 'gogs@april.com'
+    },
+    {
+        id: 8,
+        name: 'Haythm Kenway',
+        username: 'mastertempler',
+        email: 'hyk@may.com'
+    },
+    {
+        id: 9,
+        name: 'Edward Kenway',
+        username: 'masterassassin',
+        email: 'edk@june.com'
+    },
+]
